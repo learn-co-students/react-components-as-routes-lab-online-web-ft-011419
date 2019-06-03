671065cd2bf2856be8d69824fe8d1d7f44d4076f
gbs4ever <gbs4ever2@gmail.com>
2019-06-03 02:38:10 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { movies } from '../data';
-
+//movies.map()
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div>
+          <h3>{movie.title}</h3>
+          <h3>{movie.time}</h3>
+         <ul>
+          {movie.genres.map(genre =>(
+            <li>{genre}</li>
+           
+          ) )}
+          </ul>
+        </div>))}
     </div>
   );
 };
